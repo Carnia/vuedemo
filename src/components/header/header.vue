6f214ff987faacf54eba10a42105ec7029d0dab6
xlqdys <xlqdys@foxmail.com>
2017-04-13 13:49:15 +0800

    test1

---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="header">
+  这是header
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.header
+  text-align:center
+  font-size:20px
+</style>
